d4ca802e24de9679e06fc97366bce97fff20e292
Diana Matu <diana.matu@ciencias.unam.mx>
2025-08-30 21:19:39 -0600

    feat: implement Thompson's construction methods

--- src/main/java/com/compiler/lexer/nfa/NFA.java
+++ src/main/java/com/compiler/lexer/nfa/NFA.java
@@ -24,8 +24,9 @@ public class NFA {
      * @param end The final (accepting) state.
      */
     public NFA(State start, State end) {
-        // TODO: Implement constructor
-        throw new UnsupportedOperationException("Not implemented");
+        this.startState = start;
+        this.endState = end;
+        this.endState.isFinal = true; // marcamos el estado final como aceptador
     }
 
     /**
@@ -34,6 +35,104 @@ public class NFA {
      */
     public State getStartState() {
     // TODO: Implement getStartState
-    throw new UnsupportedOperationException("Not implemented");
+        return startState;
+    }
+
+     /**
+     * Creates a basic NFA for a single symbol
+     * @param c transition symbol
+     */
+    public static NFA basic(char c) {
+        State start = new State();
+        State end = new State();
+        end.isFinal = true;
+        start.transitions.add(new Transition(c, end));
+        return new NFA(start, end);
+    }
+
+ /**
+     * Concatenation of two NFAs (A·B).
+     */
+    public static NFA concatenate(NFA a, NFA b) {
+        // The final state of A is no longer final, and it connects to B's start state with epsilon       a.endState.isFinal = false;
+        a.endState.transitions.add(new Transition(null, b.startState));
+        return new NFA(a.startState, b.endState);
+    }
+
+    /**
+     * Unión of two NFAs (A|B).
+     */
+    public static NFA union(NFA a, NFA b) {
+        State start = new State();
+        State end = new State();
+        end.isFinal = true;
+
+        // new epsilon-link
+        start.transitions.add(new Transition(null, a.startState));
+        start.transitions.add(new Transition(null, b.startState));
+
+        a.endState.isFinal = false;
+        b.endState.isFinal = false;
+
+        a.endState.transitions.add(new Transition(null, end));
+        b.endState.transitions.add(new Transition(null, end));
+
+        return new NFA(start, end);
+    }
+
+    /**
+     * CKleene star clousure (A*).
+     */
+    public static NFA kleeneStar(NFA a) {
+        State start = new State();
+        State end = new State();
+        end.isFinal = true;
+
+        start.transitions.add(new Transition(null, a.startState)); // ir a A
+        start.transitions.add(new Transition(null, end));          // o aceptar vacío
+
+        a.endState.isFinal = false;
+        a.endState.transitions.add(new Transition(null, a.startState)); // repetir A
+        a.endState.transitions.add(new Transition(null, end));          // o ir a fin
+
+        return new NFA(start, end);
+    }
+
+    /**
+     * Operator + 
+     */
+    public static NFA plus(NFA a) {
+        State start = new State();
+        State end = new State();
+        end.isFinal = true;
+
+        // start → A
+        start.transitions.add(new Transition(null, a.startState));
+
+        // A.end → A.start (to repeat)
+        a.endState.isFinal = false;
+        a.endState.transitions.add(new Transition(null, a.startState));
+
+        // A.end → end
+        a.endState.transitions.add(new Transition(null, end));
+
+        return new NFA(start, end);
+    }
+
+    /**
+     * Operator ? (zero or occurrence).
+     */
+    public static NFA optional(NFA a) {
+        State start = new State();
+        State end = new State();
+        end.isFinal = true;
+
+        start.transitions.add(new Transition(null, a.startState)); // tomar A
+        start.transitions.add(new Transition(null, end));          // o ir directo a end
+
+        a.endState.isFinal = false;
+        a.endState.transitions.add(new Transition(null, end));
+
+        return new NFA(start, end);
     }
 }
